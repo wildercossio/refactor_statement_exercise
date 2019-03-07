3a173a93ddd0bc2668fe15177466a4bff2c31724
Wilder Cossio <w.cossio.r@gmail.com>
2019-03-07 03:11:13 -0400

    funcion format creada

--- statement.js
+++ statement.js
@@ -2,11 +2,6 @@ function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat("en-US",
-    {
-      style: "currency", currency: "USD",
-      minimumFractionDigits: 2
-    }).format;
   for (let perf of invoice.performances) {
 
     volumeCredits += volumeCreditsFor(perf);
@@ -50,6 +45,13 @@ function statement(invoice, plays) {
     if ("comedy" === playFor(aPerformance).type) result +=
       Math.floor(aPerformance.audience / 5);
     return result;
+  }
+  function format(aNumber) {
+    return new Intl.NumberFormat("en-US",
+      {
+        style: "currency", currency: "USD",
+        minimumFractionDigits: 2
+      }).format(aNumber);
   }
 }
 
